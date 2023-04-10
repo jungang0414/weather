b437e592cb13584dcacd59995d508e2f010c4f91
jungang0414 <88829626+jungang0414@users.noreply.github.com>
2023-04-10 14:43:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'normalize.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
@@ -16,3 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
